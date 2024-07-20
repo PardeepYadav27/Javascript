e97736d9dded05c7137c2b00856f973225c2ba23
Pardeep Yadav <121375828+PardeepYadav27@users.noreply.github.com>
2024-07-20 14:30:36 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+const accoutId = 12345;
+let accoutnEmail = "abc@gmail.com";
+var accountPassword = 12345678;
+
+// accoutId = 9876560;
+accoutnEmail = "pardeep@gmail.com";
+accountPassword = 433453;
+
+console.table([accoutId,accoutnEmail,accountPassword]);
